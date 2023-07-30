8cba529f84f0e0ec12f2b933210584648777e70c
Amir Sorayaei <amir.s1377@gmail.com>
2023-07-30 17:01:09 +0330

    Added all the interfaces that needed for app

---
+++ src/types/types.ts
@@ -0,0 +1,19 @@
+/**
+ * All the interfaces
+ */
+
+export interface Home {
+  data: Post[];
+}
+
+export interface Post {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
+
+export interface AppState {
+  posts: Post[];
+  theme: "dark" | "light";
+}
