8d27c4ae787c3c71958e5650a0fb6756bc9f31fd
Amir Sorayaei <amir.s1377@gmail.com>
2023-07-30 16:51:35 +0330

    Added all needed assets

---
+++ src/assets/fonts/Fonts.ts
@@ -0,0 +1,14 @@
+/**
+ * This file includes all the fonts
+ */
+
+import { Montserrat } from "next/font/google";
+
+/**
+ * Montserrat font
+ */
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+  display: "swap",
+});
