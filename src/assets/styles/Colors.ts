8ae1a85ad8104899647218e04acb7a7e44c60229
Amir Sorayaei <amir.s1377@gmail.com>
2023-07-30 17:05:33 +0330

    Bug fixes on navy color

--- src/assets/styles/Colors.ts
+++ src/assets/styles/Colors.ts
@@ -5,5 +5,5 @@
 export const pink = "#ffa7c4";
 export const white = "#fff";
 export const black = "#222";
-export const navy = "282c35";
+export const navy = "#282c35";
 export const bright_pink = "#d23669";
